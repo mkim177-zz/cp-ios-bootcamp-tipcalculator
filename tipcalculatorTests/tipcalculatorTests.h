cbc1660cadabeb5e471665c49326e01e9d1cb92c
Mark Kim <mkim@zynga.com>
2013-10-01 16:41:13 -0700

    Initial Commit

---
+++ tipcalculatorTests/tipcalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  tipcalculatorTests.h
+//  tipcalculatorTests
+//
+//  Created by mkim on 10/1/13.
+//  Copyright (c) 2013 mkim. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface tipcalculatorTests : SenTestCase
+
+@end
---
+++ tipcalculatorTests/tipcalculatorTests.m
@@ -0,0 +1,32 @@
+//
+//  tipcalculatorTests.m
+//  tipcalculatorTests
+//
+//  Created by mkim on 10/1/13.
+//  Copyright (c) 2013 mkim. All rights reserved.
+//
+
+#import "tipcalculatorTests.h"
+
+@implementation tipcalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in tipcalculatorTests");
+}
+
+@end
